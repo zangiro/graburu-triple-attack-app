d8c1bb4e3dd23b5cad18f70edb080f9fc5f29b0d
ヨシアキ <yoshi@yoshiakis-MacBook-Air.local>
2025-06-24 21:04:39 +0900

    ログインページ作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,17 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
+  #sorceryによる認証機能を持たせる
+
+  validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
+  # if => {}:   「新しいレコード」または「crypted_passwordが変更された」
+  # passwordの長さが最低3文字以上であることを要求する
+
+  validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
+  # password_confirmationという属性がpasswordと一致することを要求するバリデーション
+
+  validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
+  # passwordフィールドの確認（再入力）が必要というバリデーションを追加してる
+
+  validates :email, presence: true, uniqueness: true
+  validates :name, presence: true, length: { maximum: 255 }
 end
