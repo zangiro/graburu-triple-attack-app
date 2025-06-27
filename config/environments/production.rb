7a39444af8a88644f0ea306edff9d95d8d9b06c8
ヨシアキ <yoshi@yoshiakis-MacBook-Air.local>
2025-06-27 21:46:08 +0900

    微修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,7 +90,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              "smtp.gmail.com",
     port:                 587,
-    domain:               "kininaru-gurume.com",
+    domain:               "myapp-old-hill-1313.fly.dev",
     user_name:            ENV["GMAIL_USERNAME"],
     password:             ENV["GMAIL_PASSWORD"],
     authentication:       "plain",
